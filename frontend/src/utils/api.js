4768c7f4fbde9333fb8a7a0e994a75025e6bdb16
hrithik7-class <kesarwaniraj11@gmail.com>
2025-08-05 20:18:04 +0530

    chnages

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,17 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+//   withCredentials: true // Send cookies with requests
+// });
+
+// export default api;
+
+
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  baseURL: '/api',
   withCredentials: true // Send cookies with requests
 });
 
